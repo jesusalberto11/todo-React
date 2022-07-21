c3ba2b4f8fe26534fc8c4b05eb780299208f2759
Jesus Morales <jesusalbertomoralesrico@gmail.com>
2022-07-21 16:06:05 -0500

    Feature: Added about alert

--- src/Components/AboutAlert.js
+++ src/Components/AboutAlert.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { Alert } from "react-native";
 
 export const AboutAlert = () => {
-  return alert("This is a button!");
+  return Alert.alert(
+    "About this app",
+    `App created by: Jesus Alberto. \n\nUsing React Native + Expo CLI \n\nVersion 1.0\n\nGithub: jesusalberto11`
+  );
 };
